447cb873a51af8aa3f9e5d33bdb553af5cf94dce
KAIO JUNCKES DILLI <kaio_dilli@estudante.sesisenai.org.br>
2026-06-29 12:00:15 -0300

    ajustandos erros pra rodar

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -2,23 +2,30 @@ const prisma = require('../prisma');
 
 exports.criarAluno = async (req, res) => {
     try {
-        console.log("🔥 CHEGOU NO CONTROLLER");
-
         const { nome, email } = req.body;
 
         const aluno = await prisma.aluno.create({
-            data: {
-                nome,
-                email
-            }
+            data: { nome, email }
         });
-
-        console.log("🔥 SALVOU NO BANCO", aluno);
 
         return res.status(201).json(aluno);
 
     } catch (error) {
-        console.log("❌ ERRO:", error.message);
         return res.status(500).json({ error: error.message });
     }
+};
+
+exports.listarAlunos = async (req, res) => {
+    const alunos = await prisma.aluno.findMany();
+    return res.json(alunos);
+};
+
+exports.buscarAluno = async (req, res) => {
+    const { id } = req.params;
+
+    const aluno = await prisma.aluno.findUnique({
+        where: { id: Number(id) }
+    });
+
+    return res.json(aluno);
 };
